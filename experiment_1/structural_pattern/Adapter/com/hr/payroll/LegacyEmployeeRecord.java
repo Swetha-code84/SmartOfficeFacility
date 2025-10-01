46f9e6292b7a8a3b6a99372230775fd206638b95
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 16:12:05 +0530

    Update LegacyEmployeeRecord.java

--- experiment_1/structural_pattern/Adapter/com/hr/payroll/LegacyEmployeeRecord.java
+++ experiment_1/structural_pattern/Adapter/com/hr/payroll/LegacyEmployeeRecord.java
@@ -1,12 +1,9 @@
 package com.hr.payroll;
-
 public class LegacyEmployeeRecord {
     private String rawData;
-
     public LegacyEmployeeRecord(String rawData) {
         this.rawData = rawData;
     }
-
     public String getRawData() {
         return rawData;
     }
