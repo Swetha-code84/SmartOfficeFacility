5bfdbf9fe8412dd353732863ba08b12c230f68c7
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 16:15:00 +0530

    Update Notifier.java

--- experiment_1/structural_pattern/Decorator/com/notification/core/Notifier.java
+++ experiment_1/structural_pattern/Decorator/com/notification/core/Notifier.java
@@ -1,6 +1,4 @@
 package com.notification.core;
-
-// 1. Component Interface: Defines the common method for all notifiers and decorators
 public interface Notifier {
     void send(String message);
 }
