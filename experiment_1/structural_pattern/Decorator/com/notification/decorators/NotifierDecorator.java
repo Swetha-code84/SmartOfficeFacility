7e8e914e24548957724ecbc4add01f83881e9d03
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 16:19:14 +0530

    Update NotifierDecorator.java

--- experiment_1/structural_pattern/Decorator/com/notification/decorators/NotifierDecorator.java
+++ experiment_1/structural_pattern/Decorator/com/notification/decorators/NotifierDecorator.java
@@ -1,18 +1,10 @@
 package com.notification.decorators;
-
 import com.notification.core.Notifier;
-
-// 3. Abstract Decorator: Holds a reference to the wrapped object and delegates the call
 public abstract class NotifierDecorator implements Notifier {
-
-    // Reference to the Component (or another Decorator)
     protected Notifier wrappedNotifier;
-
     public NotifierDecorator(Notifier wrappedNotifier) {
         this.wrappedNotifier = wrappedNotifier;
     }
-
-    // Delegates the core functionality
     @Override
     public void send(String message) {
         wrappedNotifier.send(message);
