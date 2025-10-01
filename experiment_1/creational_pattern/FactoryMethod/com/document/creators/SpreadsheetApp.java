10ecf4d306e8a3b6b7185dd6202bc67e690c4a84
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 16:05:03 +0530

    Update SpreadsheetApp.java

--- experiment_1/creational_pattern/FactoryMethod/com/document/creators/SpreadsheetApp.java
+++ experiment_1/creational_pattern/FactoryMethod/com/document/creators/SpreadsheetApp.java
@@ -1,22 +1,16 @@
 package com.document.creators;
-
 import com.document.products.IDocument;
 import com.document.products.SpreadsheetDocument;
-
 public class SpreadsheetApp extends DocumentApplication {
-
     @Override
     public IDocument createDocument(String name) {
         System.out.println("[FACTORY METHOD]: Creating SpreadsheetDocument object...");
         return new SpreadsheetDocument(name);
     }
-
     @Override
     public String getApplicationType() {
         return "Spreadsheet";
     }
-
-    // NEW: Provides the unique starting content for Spreadsheet Documents
     @Override
     public String getDefaultContent() {
         return "Default 3 columns, 10 rows, ready for data entry.";
