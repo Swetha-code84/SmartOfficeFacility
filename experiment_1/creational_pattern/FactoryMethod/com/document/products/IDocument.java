2a4c9efcede2db24b72aa8748eb3d27c6c55f50a
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 16:06:50 +0530

    Update IDocument.java

--- experiment_1/creational_pattern/FactoryMethod/com/document/products/IDocument.java
+++ experiment_1/creational_pattern/FactoryMethod/com/document/products/IDocument.java
@@ -1,10 +1,7 @@
 package com.document.products;
-
 public interface IDocument {
     void open();
     void save();
     void close();
-
-    // NEW: Method to initialize the document with content
     void initialize(String content);
 }
