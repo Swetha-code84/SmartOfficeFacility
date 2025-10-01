ef67f1fbc43fd2d4d7d536ea0051f8479e42f6ed
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 15:53:30 +0530

    Update IServiceColleague.java

--- experiment_1/behavioral_pattern/Mediator/com/transaction/colleagues/IServiceColleague.java
+++ experiment_1/behavioral_pattern/Mediator/com/transaction/colleagues/IServiceColleague.java
@@ -1,13 +1,8 @@
 package com.transaction.colleagues;
-
 import com.transaction.mediator.ITransactionCoordinator;
-
-// Colleague Interface
 public interface IServiceColleague {
     String getName();
     void setCoordinator(ITransactionCoordinator coordinator);
-
-    // UPDATED SIGNATURE
     boolean prepare(String transactionId, boolean forceFailure);
     void commit(String transactionId);
     void rollback(String transactionId);
