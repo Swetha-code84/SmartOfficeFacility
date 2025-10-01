25258d10d3d80410591e965257bbaf2b478ef06f
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 15:33:56 +0530

    Update IControlObserver.java

--- experiment_2/interfaces/IControlObserver.java
+++ experiment_2/interfaces/IControlObserver.java
@@ -2,18 +2,7 @@ package com.smartoffice.facility.interfaces;
 
 import com.smartoffice.facility.core.Room;
 
-/**
- * The IControlObserver interface in the Observer Pattern.
- * It defines the update method that control systems (Observers) must implement
- * to react to changes in the Room's state (Subject).
- */
 public interface IControlObserver {
 
-    /**
-     * Called by the Subject (Room) to notify the Observer of a state change.
-     * The Observer uses the information in the Room object to decide what action to take.
-     *
-     * @param room The Room object whose state has changed.
-     */
     void update(Room room);
 }
