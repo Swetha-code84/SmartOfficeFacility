10e7a877246df14f3d6efc100bf9b92e40a736fc
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 16:01:37 +0530

    Update IDiscountStrategy.java

--- experiment_1/behavioral_pattern/Strategy/com/discount/strategies/IDiscountStrategy.java
+++ experiment_1/behavioral_pattern/Strategy/com/discount/strategies/IDiscountStrategy.java
@@ -1,6 +1,4 @@
 package com.discount.strategies;
-
-// 1. Strategy Interface: Defines the common contract for all discount algorithms
 public interface IDiscountStrategy {
     String getName();
     double calculateDiscount(double originalTotal);
