dc0e23a970094b9b7a0460a1baf291f323262ff7
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 16:05:27 +0530

    Update TextEditor.java

--- experiment_1/creational_pattern/FactoryMethod/com/document/creators/TextEditor.java
+++ experiment_1/creational_pattern/FactoryMethod/com/document/creators/TextEditor.java
@@ -1,22 +1,16 @@
 package com.document.creators;
-
 import com.document.products.IDocument;
 import com.document.products.TextDocument;
-
 public class TextEditor extends DocumentApplication {
-
     @Override
     public IDocument createDocument(String name) {
         System.out.println("[FACTORY METHOD]: Creating TextDocument object...");
         return new TextDocument(name);
     }
-
     @Override
     public String getApplicationType() {
         return "Text";
     }
-
-    // NEW: Provides the unique starting content for Text Documents
     @Override
     public String getDefaultContent() {
         return "Start typing here...";
