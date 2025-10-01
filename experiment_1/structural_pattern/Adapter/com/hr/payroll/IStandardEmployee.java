889ecaf075c4a4c32a7be5b139a9af50a95e6b78
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 16:11:47 +0530

    Update IStandardEmployee.java

--- experiment_1/structural_pattern/Adapter/com/hr/payroll/IStandardEmployee.java
+++ experiment_1/structural_pattern/Adapter/com/hr/payroll/IStandardEmployee.java
@@ -1,9 +1,6 @@
 package com.hr.payroll;
-
 public interface IStandardEmployee {
     String getFullName();
     double getYearlySalary();
-
-    // NEW METHOD: Added for the Salary Risk Assessment feature
     String getSalaryAssessment();
 }
