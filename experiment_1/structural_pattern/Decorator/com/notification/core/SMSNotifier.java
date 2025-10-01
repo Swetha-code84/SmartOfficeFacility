ae53fe65cb522f3ef3841b470d4397ccdb6bbff5
Swetha-code84 <kswetha281@gmail.com>
2025-10-01 16:15:23 +0530

    Update SMSNotifier.java

--- experiment_1/structural_pattern/Decorator/com/notification/core/SMSNotifier.java
+++ experiment_1/structural_pattern/Decorator/com/notification/core/SMSNotifier.java
@@ -1,8 +1,5 @@
 package com.notification.core;
-
-// 2. Concrete Component: The basic object we are decorating
 public class SMSNotifier implements Notifier {
-
     @Override
     public void send(String message) {
         System.out.println("CORE NOTIFIER: Sending basic SMS message: '" + message + "'");
